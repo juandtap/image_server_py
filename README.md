8ce3cacd6b76a16ea2d4096767afe700ea0d8f50
juandtap <jtapiav4@est.ups.edu.ec>
2024-05-30 02:13:43 -0500

    se agregan efectos

--- README.md
+++ README.md
@@ -2,15 +2,39 @@
 
 ## Vision por computador P64
 
-### Autor: Diego Tapia
+### Autor: Diego Tapia, Paul Astudillo (https://github.com/Paul-Astudillo)
 
 Servidor Flask para fusion de imagenes provenientes desde dos fuentes, la primera la computadora desde donde se ejecuta este servidor y la segunda desde la apliaccion en Android la cual envia la imagen procesada por sockets.
 
-### Metodo para reciobir imagenes
+### Metodo para recibir imagenes
+Desde la app de android se envia la imagen por http al endpoint http://servidorflask:5000/recepcion
+En la app de android se especifica la direccion IP del servidor flask
 
-En terminal usar:
+### Fuentes de video
+Se tiene dos fuentes de videos, un video .mov local en la raiz del proyecto, ignorado por git ( > 400Mb). 
+La segunda fuente de video es la camara de un telefono android, usado como webcam con la aplicacion DroidCam.
 
-```curl -X POST -H "Content-Type: application/json" http://localhost:5000/recepcion```
+El frontend pide la direccion IP de la "webcam"
+
+### Fusion 
+La imagen recibida desde android y el video externo con efectos, son "fusionados" con el video.
+
+### Efectos
+
+Se tienen los siguientes efectos:
+
+App Android:
+    - Fusion con textura de fuego
+    - Quitar fondo usando el espacio de colores LAB
+
+Servidor Flask:
+    - Quitar fondo
+    - Deteccion de bordes (Canny, Dilaacion, GaussianBlur)
+    
+
+
+
+
 
 
 
--- efectos.py
+++ efectos.py
@@ -3,6 +3,98 @@
 import cv2 as cv
 import numpy as np
 
+# BGR
+COLOR_BORDES = (213, 46, 130)
+
+# Parametros para Canny 
+# (se hicieron pruebas y con estos se tenia una mejor visualizacion)
+LIMITE_INF = 80
+LIMITE_SUP = 160
+
+# Parámetros para aplicar la Dilatación
+TAMANO_KERNEL = 3
+elemento = cv.getStructuringElement(cv.MORPH_CROSS, (TAMANO_KERNEL, TAMANO_KERNEL))
+
 
 def removeBackgroud(frame):
-    pass
+    # El codigo para remover el fondo fue tomado de:
+    # https://stackoverflow.com/questions/51719472/remove-green-background-screen-from-image-using-opencv-python
+    
+    # suavizado para reduccion de ruido
+    frame = cv.GaussianBlur(frame, (5,5), sigmaX=3, sigmaY=3, borderType = cv.BORDER_DEFAULT)
+    # se usa el espacio de color LAB
+    lab = cv.cvtColor(frame, cv.COLOR_BGR2LAB)
+    
+    # se extrae el canal A
+    A = lab[:,:,1]
+
+    thresh = cv.threshold(A, 127, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
+
+    mascara = cv.bitwise_and(frame, frame, mask=thresh)
+
+    mask1 = mascara.copy()
+
+    mask1[thresh == 0] =(255,255,255)
+
+    mask_lab = cv.cvtColor(mascara, cv.COLOR_BGR2LAB)
+
+    frame_destino = cv.normalize(mask_lab[:,:,1], dst=None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8U)
+
+    frame_destino_thresh = cv.threshold(frame_destino, 100, 255, cv.THRESH_BINARY_INV)[1]
+
+
+    mask_lab[:,:,1][frame_destino_thresh == 255] = 127
+
+    resultado = cv.cvtColor(mask_lab,cv.COLOR_LAB2BGR)
+
+    resultado[thresh == 0] = (255,255,255)
+
+    return resultado
+
+
+def bordesColor(frame):
+    # SE CONVIERTE A ESCALA DE GRISES PARA EL DETECTOR DE BORDES
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    frame_suave = cv.GaussianBlur(frame_gray, (5,5), 0)
+
+    bordes = cv.Canny(frame_suave, LIMITE_INF, LIMITE_SUP)
+
+    # se dilatan los bordes para mejor visualizacion
+
+    bordes = cv.dilate(bordes, elemento)
+
+    # agrego color a los bordes
+
+    bordes_de_color = cv.cvtColor(bordes, cv.COLOR_BGR2RGB)
+
+    imagen_resultado = np.zeros_like(bordes_de_color)
+
+    # se escogen solo los bordes, pixeles mayores a cero
+    mask = bordes > 0
+
+    imagen_resultado[mask] = COLOR_BORDES
+
+    return imagen_resultado
+
+
+
+video = cv.VideoCapture("http://192.168.100.44:4747/video")
+
+if video.isOpened():
+    while True:
+        _, frame = video.read()
+
+        frame_sin_fondo = removeBackgroud(frame)
+
+        frame_bordes = bordesColor(frame_sin_fondo)
+
+        cv.imshow("Original", frame)
+        cv.imshow("sin fondo", frame_sin_fondo)
+        cv.imshow("bordes", frame_bordes)
+        
+
+        if cv.waitKey(1) == ord('q'):
+            break
+
+
